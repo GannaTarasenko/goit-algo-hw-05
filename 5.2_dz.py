fc89c962884f0b2699f57b2b559b47ec4c647558
Hanna Tarasenko <maracheva.ganna@gmail.com>
2024-03-23 17:29:19 +0100

    5.2endend

--- 5.2_dz.py
+++ 5.2_dz.py
@@ -1,16 +1,17 @@
 import re
 from typing import Callable
+from decimal import Decimal
 
 def generator_numbers(text: str):
     pattern = r'\b\d+(\.\d+)?\b'  # Регулярний вираз для пошуку дійсних чисел
     for match in re.finditer(pattern, text):
-        yield float(match.group())  # Повертаємо кожне знайдене число як дійсне
+        yield Decimal(match.group())  # Повертаємо кожне знайдене число як десяткове
 
 def sum_profit(text: str, func: Callable):
-    total = sum(func(text))  # Використовуємо генератор для отримання всіх чисел та підсумовуємо їх
+    total = sum(func(text), Decimal('0'))  # Використовуємо генератор для отримання всіх чисел та підсумовуємо їх
     return total
 
 # Приклад використання
-text = "Встановити зарплатню Тарасенко Ганні у розмірі 2000 євро як основний дохід, доповнений премією у 500.05 євро помісячно і 325.1 євро на гарну відпустку."
+text = "Встановити зарплатню Тарасенко Ганні у розмірі 2000 євро як основний дохід, доповнений премією у 500.05 євро помісячно і 5325.1 євро на гарну відпустку."
 total_income = sum_profit(text, generator_numbers)
 print(f"Загальний дохід: {total_income}")
